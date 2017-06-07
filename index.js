0efe4eea61a8ddeef7d20e1197cd7e0db2445ca5
Heidi Anderson <heidianderson@heidis-mbp.coldspring.marian.edu>
2017-06-06 22:32:10 -0400

    look better, list not working yet

--- index.js
+++ index.js
@@ -9,7 +9,21 @@ function handleSubmit(ev)
     const movieTitle = f.movieTitle.value
     const movieYear = f.movieYear.value
 
-    details.innerHTML=`<li>${movieTitle} (${movieYear})</li>`
+    const titleArray = movieTitle.split(' ')
+    let i=0
+    let titleWithDashes = ''
+    for(i=0;i<titleArray.length-1;i++)
+    {
+        titleWithDashes+=titleArray[i]+'-'
+    }
+    titleWithDashes+=titleArray[i]
+
+details.appendChild(`
+<div class="list-group">
+    <a href="http://www.imdb.com/find?ref_=nv_sr_fn&q=${titleWithDashes}&s=all" class="list-group-item">${movieTitle} (${movieYear})</a>
+    <button type="Submit" class="btn btn-outline-info waves-effect">Go</button>
+</div>
+`)
 }
 
 listForm.addEventListener('submit', handleSubmit)
